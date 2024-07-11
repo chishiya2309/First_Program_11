b1c650a219302cf6b638f282163079021a842ed7
nvbao117 <baocute056@gmail.com>
2024-07-11 17:06:38 +0700

    add:subtraction.cs

---
+++ subtraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhập số thứ nhất: ");
+        string input1 = Console.ReadLine();
+        double a = double.Parse(input1);
+        Console.Write("Nhập số thứ hai: ");
+        string input2 = Console.ReadLine();
+        double b = double.Parse(input2);
+        double result = a - b;
+        Console.WriteLine("Kết quả của " + a + " - " + b + " = " + result);
+    }
+}
